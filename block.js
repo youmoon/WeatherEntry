be16f5b1b6a3d1e8ffda754a384c7e48dee86064
Yeomoon <73222399+youmoon@users.noreply.github.com>
2023-01-26 09:58:47 +0900

    Update block.js

--- block.js
+++ block.js
@@ -1,3 +1,3 @@
-alert("⛅ WeatherEntry Plugin Version 1.0\nPlugin by Yeomoon");
 console.log("Success at load WeatherEntry Plugin Version 1.0\n플러그인 적용이 완료되었습니다.");
 document.title = "⛅ WeatherEntry";
+alert("⛅ WeatherEntry Plugin Version 1.0\nPlugin by Yeomoon");
